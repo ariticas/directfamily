62c141e2317bf43b03987506a8fde00f6bb8deee
ariticas10 <ariticas10@gmail.com>
2020-10-26 19:44:53 -0700

    sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/contact'
-  get 'static_pages/about'
-  get 'static_pages/blog'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  get 'users/new'
+    root 'static_pages#home'
+    get '/help', to: 'static_pages#help'
+    get '/blog' , to: 'static_pages#blog'
+    get '/about', to: 'static_pages#about'
+    get '/contact', to: 'static_pages#contact'
+    
+    get '/signup' , to: 'users#new'
+   
+    resources :users
 end
