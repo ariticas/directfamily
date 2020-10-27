47d63c6ad9c3e1716eb86728d328b0121819da60
ariticas10 <ariticas10@gmail.com>
2020-10-27 13:17:07 -0700

    make a model

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,10 +1,12 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
+
+test "should get home" do
     get static_pages_home_url
     assert_response :success
-  end
+end
+
 
   test "should get contact" do
     get static_pages_contact_url
