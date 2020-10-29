a07ba899264ff7ff36dca63572bd0684df4a92af
ariticas10 <ariticas10@gmail.com>
2020-10-29 11:35:56 -0700

    sessions stuff

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+      user = User.find_by(email: params[:session][:email].downcase)
+      if user && user.authenticate(params[:session][:password])
+        #this prevents session fixation atack nby reseting session before loggin in to clear ID and data
+        reset_session
+        log_in User
+        redirect_to user
+      else
+         #flash.now will dissapaer when new request is made unlike flash that dissapears on the second request
+        flash.now[:danger] = 'Invalid email/password combination'
+        render 'new'
+    end
+  end
+  
+  def destroy
+  end
+end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,23 +3,23 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
 test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
 end
 
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
   end
 
   test "should get blog" do
-    get static_pages_blog_url
+    get blog_path
     assert_response :success
   end
 
