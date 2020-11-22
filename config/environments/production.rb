0048e415215c082e1919363c75a627332dc2a72d
ariticas10 <ariticas10@gmail.com>
2020-11-21 20:40:13 -0800

    d

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://projectelliebear.herokuapp.com'
+  host = 'https://blooming-eyrie-19461.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
